e581a19a2c315991000da78050bdaee7295e5b62
Daniel Lewandowski <daniel.lewandowski@outlook.com>
2022-12-01 19:35:49 +0100

    day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,36 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val energy = input
+            .map(String::trim)
+            .fold(Pair(listOf<Int>(), 0)) { acc, t ->
+                if (t.trim().isEmpty()) {
+                    Pair(acc.first + acc.second, 0)
+                } else {
+                    Pair(acc.first, acc.second + t.toInt())
+                }
+            }
+
+        return energy.first.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val energy = input
+            .map(String::trim)
+            .fold(Pair(listOf<Int>(), 0)) { acc, t ->
+                if (t.trim().isEmpty()) {
+                    Pair(acc.first + acc.second, 0)
+                } else {
+                    Pair(acc.first, acc.second + t.toInt())
+                }
+            }
+
+        return energy.first.sorted().takeLast(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 72478)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput("Day01_test")
+    check(part2(testInput) == 210367)
 }
